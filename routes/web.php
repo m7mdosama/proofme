c2e83ef2ef0368915102995260549022346c62d1
Mohamad <m7md.osama@yahoo.com>
2018-04-30 13:40:02 +0200

    Prject test 2

--- routes/web.php
+++ routes/web.php
@@ -13,22 +13,25 @@
 
 use App\Role;
 
-//Route::get('/ir',function(){
-//    //App\Role::create(['name'=>'User','description'=>'test 2']);
-//
-//    //$x = App\Role::find(4);
-//    //if($x->delete())
-//    //    echo "Good <br>";
-//
-//    //echo Role::all();
-//
-//    return view('welcome');
-//});
+Route::get('/ir',function(){
+    //App\Role::create(['name'=>'User','description'=>'test 2']);
+
+    //$x = App\Role::find(4);
+    //if($x->delete())
+    //    echo "Good <br>";
+
+    //echo Role::all();
+
+    return view('welcome');
+});
 
 Auth::routes();
 
 //Route::get('/home', 'HomeController@index')->name('home');
 
+Route::get('/',function (){
+    return view('welcome');
+});
 //Route::get('/', 'DashboardController@index')->name('index');
 Route::get('/dashboard', 'DashboardController@index')->name('dashboard');
 Route::post('/upload', 'DashboardController@uploadDesign')->name('uploadDesign');
